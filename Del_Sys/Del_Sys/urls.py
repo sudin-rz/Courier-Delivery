9ea68bddfdb6070b137173d22d297ed1411b25a0
Sudin Rajbhandari <sudin.rajbandari@deerwalk.edu.np>
2024-04-11 20:30:23 +0545

    Courier Accept Job

--- Del_Sys/Del_Sys/urls.py
+++ Del_Sys/Del_Sys/urls.py
@@ -24,9 +24,10 @@ customer_urlpatterns=[
 courier_urlpatterns=[
     path('',courier_views.home, name="home"),
     path('jobs/available/',courier_views.available_jobs_page, name="available_jobs"),
-
+    path('jobs/available/<id>/',courier_views.available_job_page, name="available_job"),
     path('api/jobs/available/',courier_apis.available_jobs_api, name="available_jobs_api"),
 ]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Del_Sys/main/courier/views.py
+++ Del_Sys/main/courier/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
+from main.models import *
+
 # Create your views here.
 
 
@@ -14,3 +16,21 @@ def home(request):
 def available_jobs_page(request):
     return render(request,'courier/available_jobs.html')
 
+
+@login_required(login_url="/sign_in/?next=/courier/")
+def available_job_page(request, id):
+    job = Job.objects.filter(id=id, status=Job.PROCESSING_STATUS).last()
+
+    if not job:
+        return redirect(reverse('courier:available_jobs'))
+
+    if request.method=='POST':
+        job.courier=request.user.courier
+        job.status = Job.PICKING_STATUS
+        job.save()
+
+        return redirect(reverse('courier:available_jobs'))
+
+    return render(request,'courier/available_job.html', {
+        "job": job
+    })
